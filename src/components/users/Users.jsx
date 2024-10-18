6f98bcce268ec3012f6376a3f56aa05bea54303c
Petar Nenov <petarnenovpetrov@gmail.com>
2024-10-18 10:44:36 +0300

    Update Users.jsx

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -9,7 +9,7 @@ const Users = () => {
 	const { data: users, error, isLoading } = useQuery({ queryKey: apiEndpoints.fetchUsers.key, queryFn: apiEndpoints.fetchUsers.fn });
 
 	if (isLoading) return <div>Loading...</div>;
-	if (error) return <div>Error loading users</div>;
+	if (error) return <div>Error loading users from jsonplaceholder</div>;
 
 	return (
 		<div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
