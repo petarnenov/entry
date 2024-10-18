0c13062b567bbc8cbdd0d590bb1906747ee589e0
Petar Nenov <petarnenovpetrov@gmail.com>
2024-10-18 23:36:52 +0300

    fix: comments

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -6,9 +6,6 @@ const useCounter = (initialValue = 1) => {
 	const increment = () => setCount(count=>count + 1);
 	const decrement = () => setCount(count=>count - 1);
 
-	// console.log("This is a code smell for testing purposes");
-	const sum = (a, b) => a - b;
-
 	return { count, increment, decrement };
 };
 
